1710948cde1b1915d989f58cded144f7c9f39633
Akash <akash.singh670@gmail.com>
2020-10-28 16:17:25 +0530

    [Akash]. (Added) UC_13_Ability to Read Or write the address book with person contact into a file using File IO

--- Day_20_Assignment_Address_Book/AddressBook.cs
+++ Day_20_Assignment_Address_Book/AddressBook.cs
@@ -6,9 +6,11 @@
 // --------------------------------------------------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
+using System.Text.RegularExpressions;
+using System.Transactions;
+using System.Xml.Serialization;
 
 namespace Day_20_Assignment_Address_Book
 {
@@ -229,6 +231,14 @@ namespace Day_20_Assignment_Address_Book
                }
                 Console.WriteLine();
             }
-        }       
+        }
+
+        internal void ReadAllText()
+        {
+            string path = @"C:\Users\Lenovo\source\repos\Day_20_Assignment_Address_Book\Hello.txt";
+            string lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+        }
     }
 }
